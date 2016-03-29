07af235594c6872a04fb8363ae04fa4a866d68b3
Jason Hamilton <kvorak@gmail.com>
2016-03-29 01:21:00 -0400

    Set up unit tests for `lpm` and the git module
    *  Decided to test the git module by creating an empty (init --bare) repository and turning it into a tarball.  That way, it can be used as a reference in unit tests to assert that an
    operation who's sole purpose was to create a files suceeded.
    *  Added a `fixtures` folder containing the tarball archive of the bare repo (`git_bare.tar`)
    *  Created an extension of `unittest.TestCase` that stubs out what we would need to add to parent class to support the use of those fixtures.
    *  Stubbed two new assertions (`assertDirectoryEquals` and `assertDirectoryContains`) that should be implmented to perform the comparison.
    
    The idea is that `tests.test_git.TestGitModule` would inherit from `lpm_unittest.LPMTestCase` instead of `unittest.TestCase`.  That way, it can contain a test that executes a
    function where the expected output isn't verified by a return value, but by a comparison of a directory tree.  An assertion could be made with `os.walk`, but the test would still
    have to hard-code the expected result and that doesn't do anything for legibility and becomes a bit of a maintenance headache.  Better would be if we could describe the expected
    result in a file that could be included with the unittest and compare the actual directory structure (via `os.walk`) to that.  This allows us to just update the expected result by
    generating it once, confirming it by hand and just commmitting it.
    
    I should point that this has been inspired by the need to ensure that my extension to the git python module is creating the correct kinds of bare repsoitories and things.

---
+++ py_lpm/modules/tests/lpm_unittest.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import unittest
+import os
+
+class LPMTestCase(unittest.TestCase):
+    """ Extends the standard `unittest.TestCase` to support LPM's fixtures and special assertions 
+
+    One of the fuctions of `LPMTestCase` is to build an extension to `unittest.TestCase` that provides
+    a greater range of tests that can be performed.  The first of these is a fixtures function that
+    gives the test case access to files that can be provided as expected output.
+
+    TODO: allow `_fixture_root` to inherit its value from a parent TestSuite.  I imagine that at 
+    some point, we will want to be able to group these in a fashion that is independant of the 
+    file system.
+    """
+
+    _fixture_root = '/fixtures/'    # always defaults to a path relative to the file being executed
+
+    @property
+    def fixtures(self):
+        """ Returns a dictionary mapping the contents of the path specified by `self._fixture_root` 
+
+        >>> self.fixtures['readme.md'].read()
+        Contents of `readme.md`
+        """
+
+        # A generator like the one below could probably provide this with very little effort
+        # return {item.name : file(item.path) for item in os.walk(os.path.walk(__FILE__/self._fixture_root)}
+
+        pass
+        
+    def assertDirectoryEquals(self, target_path, expected_result, message):
+        """ Adds an assertion to verify that a file path matches the structure of a tar file
+
+        It should be noted that at this point, the assertion is considered true if the target path
+        *exactly* matches the tarball.
+
+        @param target_path:  a string representation of the path to analyze.  This should be provided
+            in an os safe format for now and should always be an absoulte path.
+        @param expected result: a tar object with the tree to compare against
+        @retuns: I will have to look up how to properly extend a unittest assertion...
+        @sa: `assertDirectoryContains`
+
+        >>> self.assertDirectoryEquals('~/.lpm/', self.fixtures['git_bare.tar'])
+        """
+
+        # Assert the parameter types.  Being a unittest, I don't think duck typing is safe
+        # assert that walking the tarball and the target path both return a valid structure
+
+        pass
+
+    def assertDirectoryContains(self, target_path, expected_result, message):
+        """ As `assertDirectoryEqual` except that the target_path needs to contain *at least* the structure of the tarball """
+
+        pass
