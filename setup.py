a9fabf51a2f2b9bdb98878451b7701c27787f99a
Jason Hamilton <kvorak@gmail.com>
2016-10-12 05:41:51 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lpm',
+      version='0.1',
+      description='A Python implementation of a Local Project Manager application',
+      url='http://github.com/Project-0/lpm',
+      author='Kvorak',
+      author_email='kvorak@gmail.com',
+      license='MIT',
+      packages=['lpm'],
+      zip_safe=False)
